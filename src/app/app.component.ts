8626051a37c2506e400e44a7ccfc94c8c86242f6
AnnaLaptop <anna.laskowska@hotmail.com>
2020-05-24 13:22:59 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'books2';
+  constructor() {
+    const firebaseConfig = {
+    apiKey: "AIzaSyDeJZWw4Zo8YOmrbwWHBFOMJMjepyWnNBs",
+    authDomain: "books2-4f5d8.firebaseapp.com",
+    databaseURL: "https://books2-4f5d8.firebaseio.com",
+    projectId: "books2-4f5d8",
+    storageBucket: "books2-4f5d8.appspot.com",
+    messagingSenderId: "544235117001",
+    appId: "1:544235117001:web:fe5297b84655f8f3d87018",
+    measurementId: "G-NQJMHQ2MS0"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+
+  }
 }
